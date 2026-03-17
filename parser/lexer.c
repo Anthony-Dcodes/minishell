ec99928043ba1fd523015abd7f2dd6638242f462
Anthony <anthony@macbook-pro.local>
2026-03-17 18:43:42 +0100

    fix index finer helpers

--- parser/lexer.c
+++ parser/lexer.c
@@ -6,7 +6,7 @@
 /*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 22:08:51 by advorace          #+#    #+#             */
-/*   Updated: 2026/03/16 22:52:16 by advorace         ###   ########.fr       */
+/*   Updated: 2026/03/17 18:40:53 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ int	find_start_index(char *str, int start)
 		}
 		return (start);
 	}
+	return (-1);
 }
 
 int find_end_index(char *str, int start, t_tokens *tokens)
@@ -49,16 +50,15 @@ int find_end_index(char *str, int start, t_tokens *tokens)
 	end = start + 1;
 	while (str[end])
 	{
-		if (tokens->quote == SINGLE_QUOTE)
-			if (str[end] == '\'')
+		if (tokens->quote == SINGLE_QUOTE && str[end] == '\'')
 				return (end);
-		else if (tokens->quote == DOUBLE_QUOTE)
-			if (str[end] == '"')
+		else if (tokens->quote == DOUBLE_QUOTE && str[end] == '"')
 				return (end);
-		else if (tokens->quote == NO_QUOTE)
-			if (str[end] == ' ')
+		else if (tokens->quote == NO_QUOTE && str[end] == ' ')
 				return (end);
 		++end;
 	}
-	return (ERR_SYNTAX);
+	if (str[end] == 0)
+		return (-1);
+	return (-2);
 }
