60bed9f7ccd1589978886e16e9894a64bd50d379
Anthony-Dcodes <dvoracekantonin@icloud.com>
2026-03-22 14:45:49 +0100

    fix find_end_index

--- parser/string_helpers.c
+++ parser/string_helpers.c
@@ -6,7 +6,7 @@
 /*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 21:40:54 by advorace          #+#    #+#             */
-/*   Updated: 2026/03/21 11:25:33 by advorace         ###   ########.fr       */
+/*   Updated: 2026/03/22 14:39:14 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,28 @@ int	find_start_index(char *str, int start)
 int find_end_index(char *str, int start, int quote)
 {
 	int	end;
+	char c;
 
 	end = start + 1;
 	if (start == -1)
 		return (-1);
+	if (str[start] == '|')
+		return (end);
+	if (str[start] == '<' || str[start] == '>')
+	{
+		if (str[start] == str[end] && str[end])
+			return (end + 1);
+		return (end);
+	}
 	while (str[end])
 	{
-		quote = track_quote_state(quote, str[end]);
+		c = str[end];
+		quote = track_quote_state(quote, c);
+		if (quote == NO_QUOTE)
+		{
+			if (c == '<' || c == '>' || c == '|' || c == ' ')
+				return (end);
+		}
 		++end;
 	}
 	return (end);
