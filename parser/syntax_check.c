cb3314c16817caad2630c70cb2fc14f10e6a86e6
Anthony-Dcodes <dvoracekantonin@icloud.com>
2026-03-22 22:30:54 +0100

    simplify syntax checking, add tests

--- parser/syntax_check.c
+++ parser/syntax_check.c
@@ -6,7 +6,7 @@
 /*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/20 10:29:28 by advorace          #+#    #+#             */
-/*   Updated: 2026/03/22 22:03:53 by advorace         ###   ########.fr       */
+/*   Updated: 2026/03/22 22:24:47 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,38 +15,35 @@
 int check_syntax(t_token *head)
 {
 	t_token *temp;
-	int		previous;
-	int		current;
-	int		next;
+	int		current_type;
 
 	temp = head;
 	while (temp)
 	{
-		if (temp->previous == NULL) // first node
+		current_type = temp->type;
+		if (current_type == REDDIRECT)
 		{
-			if (temp->type == PIPE)
+			if (temp->next == NULL)
+				return (ERR_SYNTAX);
+			else if (temp->next->type != WORD)
 				return (ERR_SYNTAX);
 		}
-		else if (temp->next == NULL) // last node
+		else if (current_type == PIPE)
 		{
-			if (temp->type == PIPE || temp->type == REDDIRECT)
+			if (temp->previous == NULL)
+				return (ERR_SYNTAX);
+			else if (temp->previous->type != WORD)
+				return (ERR_SYNTAX);
+			else if (temp->next == NULL)
+				return (ERR_SYNTAX);
+			else if (temp->next->type != WORD)
 				return (ERR_SYNTAX);
 		}
-		else // somewhere in the middle
+		else if (current_type == WORD)
 		{
-			previous = temp->previous->type;
-			current = temp->type;
-			next = temp->next->type;
-			if (current == PIPE || current == REDDIRECT)
-			{
-				if (next == PIPE || next == REDDIRECT)
-					return (ERR_SYNTAX);
-				else if (previous == PIPE || next == PIPE)
-					return (ERR_SYNTAX);
-			}
+			if (has_unclosed_quote(temp->value))
+				return (ERR_SYNTAX);
 		}
-		if (has_unclosed_quote(temp->value))
-			return (ERR_SYNTAX);
 		temp = temp->next;
 	}
 	return (ERR_OK);
--- parser/tests/syntax_tests.h
+++ parser/tests/syntax_tests.h
@@ -6,7 +6,7 @@
 /*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/22 18:20:16 by advorace          #+#    #+#             */
-/*   Updated: 2026/03/22 21:40:50 by advorace         ###   ########.fr       */
+/*   Updated: 2026/03/22 22:27:41 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,30 @@ t_test tests[] = {
 	{"echo \"hel''lo", ERR_SYNTAX},
 	{"echo hello'rorld", ERR_SYNTAX},
 	{"echo \"hello'\"'", ERR_SYNTAX},
+	// Redirect at beginning (valid — your checker allows it)
+	{"< file cat", ERR_OK},
+	{"< file", ERR_OK},
+	{"> file", ERR_OK},
 
+	// Chained pipes
+	{"cat | grep foo | wc -l", ERR_OK},
+	{"cat | grep | wc | head", ERR_OK},
+
+	// Pipe next to redirect
+	{"cat | > file", ERR_SYNTAX},     // pipe with redirect after, no word
+	{"cat > file | grep", ERR_OK},    // redirect mid-pipeline, valid
+
+	// Empty quotes (valid — empty string token)
+	{"echo ''", ERR_OK},
+	{"echo \"\"", ERR_OK},
+	{"echo ''\"\"", ERR_OK},
+
+	// Heredoc edge cases
+	{"cat << EOF | grep foo", ERR_OK},
+	{"cat <<", ERR_SYNTAX},
+
+	// Redirect followed by redirect
+	{"cat >> >> file", ERR_SYNTAX},
+	{"cat < < file", ERR_SYNTAX},
 	{NULL, 0}
 };
