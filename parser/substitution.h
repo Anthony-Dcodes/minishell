1a9fc9a879b5778580d71d8c5961635585338b77
Anthony-Dcodes <dvoracekantonin@icloud.com>
2026-05-21 17:56:33 +0200

    add env_whitelist_char()

--- parser/substitution.h
+++ parser/substitution.h
@@ -6,10 +6,11 @@
 /*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 16:49:13 by advorace          #+#    #+#             */
-/*   Updated: 2026/05/21 16:52:49 by advorace         ###   ########.fr       */
+/*   Updated: 2026/05/21 17:55:44 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-int	env_start_name_valid(char c);
+int	env_first_char_valid(char c);
+int env_whitelist_char(char c);
--- parser/substitution_utils.c
+++ parser/substitution_utils.c
@@ -6,14 +6,24 @@
 /*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 16:48:53 by advorace          #+#    #+#             */
-/*   Updated: 2026/05/21 16:52:27 by advorace         ###   ########.fr       */
+/*   Updated: 2026/05/21 17:55:35 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	env_start_name_valid(char c)
+int	env_first_char_valid(char c)
 {
 	if ((c >= 'A' && c <= 'Z')
 		|| c >= 'a' && c <= 'z'
+		|| c == '_')
+		return (1);
+	return (0);
+}
+
+int env_whitelist_char(char c)
+{
+	if ((c >= 'A' && c <= 'Z')
+		|| (c >= 'a' && c <= 'z')
+		|| (c >= '0' && c <= '9')
 		|| c == '_')
 		return (1);
 	return (0);
