06b8d6d512346ec545f4c462c6fc4e9f065b43b5
Anthony-Dcodes <dvoracekantonin@icloud.com>
2026-05-23 14:53:18 +0200

    more quote tests

--- parser/tests/remove_quotes_tests.h
+++ parser/tests/remove_quotes_tests.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   remove_quotes_tests.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: advorace <advorace@student.42.fr>          +#+  +:+       +#+        */
+/*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/25 00:00:00 by advorace          #+#    #+#             */
-/*   Updated: 2026/05/22 16:51:38 by advorace         ###   ########.fr       */
+/*   Updated: 2026/05/23 14:47:28 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ t_quote_remove_test quote_remove_test[] = {
 	{"echo''", "echo", 0},
 	{"\"\"\"\"echo\"\"\"\"", "echo", 0},
 	{"''''echo''''", "echo", 0},
-	
+
 	// No quotes left same quote
 	{"\"echo\"a\"echo\"", "echoaecho", 0},
 	{"\"echoaecho\"", "echoaecho", 0},
@@ -50,7 +50,45 @@ t_quote_remove_test quote_remove_test[] = {
 	// Quote in the middle left
 	{"\"ec'ho\"", "ec'ho", 0},
 	{"'ec\"ho'", "ec\"ho", 0},
+
+	// Empty string results (must still produce an EMPTY token, not NULL)
+	{"\"\"", "", 0},
+	{"''", "", 0},
+	{"\"\"''", "", 0},
+	{"''\"\"", "", 0},
+	{"\"\"\"\"", "", 0},
+	{"''''", "", 0},
+	{"a\"\"b", "ab", 0},          // empty quote mid-word vanishes, concatenates
+	{"a''b", "ab", 0},
+
+	// Whitespace preserved inside quotes (the whole reason quotes exist)
+	{"\"a b\"", "a b", 0},
+	{"'a b'", "a b", 0},
+	{"\"  \"", "  ", 0},
+	{"a\" \"b", "a b", 0},
+	{"\"a b\"c", "a bc", 0},
+	{"'hello world'", "hello world", 0},
+
+	// Mixed quote types concatenating into one word
+	{"\"a\"'b'", "ab", 0},
+	{"'a'\"b\"", "ab", 0},
+	{"\"a\"'b'\"c\"", "abc", 0},
+	{"'a'\"b\"'c'", "abc", 0},
+	{"x\"a\"'b'y", "xaby", 0},
+
+	// Shell metacharacters are literal inside quotes (lexer must not have split these)
+	{"\"a|b\"", "a|b", 0},
+	{"'a;b'", "a;b", 0},
+	{"\"a>b<c\"", "a>b<c", 0},
+	{"'*'", "*", 0},
+	{"\"a&&b\"", "a&&b", 0},
 	
+	// Minimal opposite-type quote preserved
+	{"\"'\"", "'", 0},
+	{"'\"'", "\"", 0},
+	{"\"a'b'c\"", "a'b'c", 0},    // a pair of single quotes literal inside double
+	{"'a\"b\"c'", "a\"b\"c", 0},
+
 	// Sentinel
 	{NULL, NULL, 0}
 };
