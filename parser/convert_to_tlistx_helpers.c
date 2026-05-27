35f362b2c70a1ecd5569e150b51a2e3f253accc8
Anthony-Dcodes <dvoracekantonin@icloud.com>
2026-05-27 12:45:34 +0200

    add allocate_t_listex_mem()

--- parser/convert_to_tlistx_helpers.c
+++ parser/convert_to_tlistx_helpers.c
@@ -6,7 +6,7 @@
 /*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 09:37:31 by advorace          #+#    #+#             */
-/*   Updated: 2026/05/27 12:10:13 by advorace         ###   ########.fr       */
+/*   Updated: 2026/05/27 12:44:43 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,5 +53,28 @@ int	assign_quote(t_listex *listex, int quote)
 	}
 	listex->xattr_qc = new_xattr_qc;
 	free(new_quote);
+	return (ERR_OK);
+}
+
+int	allocate_t_listex_mem(t_listex ***listex, t_token *head)
+{
+	int	n_pipes;
+	int	i;
+
+	i = 0;
+	n_pipes = get_n_pipes(head);
+	*listex = malloc(sizeof(t_listex*) * (n_pipes + 2));
+	if (!*listex)
+		return (ERR_MALLOC);
+	while (i < n_pipes)
+	{
+		(*listex)[i] = malloc(sizeof(t_listex));
+		if (!(*listex)[i])
+		{
+			return (ERR_MALLOC);
+		}
+		++i;
+	}
+	(*listex)[i] = NULL;
 	return (ERR_OK);
 }
