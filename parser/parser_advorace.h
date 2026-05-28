9dd894f240658c0d3d54274fa36b7e842c54f88d
Anthony-Dcodes <dvoracekantonin@icloud.com>
2026-05-28 21:26:39 +0200

    add parser_advorace.h main header file

---
+++ parser/parser_advorace.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_advorace.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/28 21:24:08 by advorace          #+#    #+#             */
+/*   Updated: 2026/05/28 21:25:31 by advorace         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "convert_to_tlistx.h"
+#include "macros.h"
+#include "parser.h"
+#include "remove_quotes.h"
+#include "string_utils.h"
+#include "structs.h"
+#include "substitution.h"
+#include "syntax_checker.h"
+#include "tokenizer.h"
