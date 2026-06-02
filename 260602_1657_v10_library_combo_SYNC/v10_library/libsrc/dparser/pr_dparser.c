029f6073f051149b468043ed34c7e0ca4507582d
AnthonyDcodes <85177751+Anthony-Dcodes@users.noreply.github.com>
2026-06-02 16:53:38 +0000

    replace epmty subst or spaces with x if its the only command between pipes

--- 260602_1657_v10_library_combo_SYNC/v10_library/libsrc/dparser/pr_dparser.c
+++ 260602_1657_v10_library_combo_SYNC/v10_library/libsrc/dparser/pr_dparser.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 14:32:16 by omayer            #+#    #+#             */
-/*   Updated: 2026/06/02 15:07:48 by codespace        ###   ########.fr       */
+/*   Updated: 2026/06/02 16:49:36 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,52 @@ static int	prepare_result(t_listex ***result, t_token *head)
 	return (SUCCESS);
 }
 
+static int replace_empty_cmds(t_listex **result)
+{
+	t_listex	**list;
+	int			i;
+	int			j;
+	int			z;
+	int			replace;
+	char		*new_str;
+
+	list = result;
+	i = 0;
+	while (list[i])
+	{
+		replace = 1;
+		j = 0;
+		while (j < (int)list[i]->size)
+		{
+			z = 0;
+			while (list[i]->items[j][z])
+			{
+				if (list[i]->items[j][z] != ' ')
+					replace = 0;
+				++z;
+			}
+			++j;
+		}
+		if (replace)
+		{
+			z = 0;
+			while (z < (int)list[i]->size)
+			{
+				new_str = malloc(sizeof(char) * 2);
+				if (!new_str)
+					return (ERR_MALLOC);
+				new_str[0] = 'x';
+				new_str[1] = '\0';
+				free(list[i]->items[z]);
+				list[i]->items[z] = new_str;
+				++z;
+			}
+		}
+		++i;
+	}
+	return (ERR_OK);
+}
+
 int	ft_eparsermain(t_s *s, char *src, t_listex ***dst, char **envp)
 {
 	int			ret;
@@ -73,8 +119,9 @@ int	ft_eparsermain(t_s *s, char *src, t_listex ***dst, char **envp)
 	ret = prepare_result(&result, head);
 	if (ret != ERR_OK)
 		return (ret);
-	print_token_metadata(head);
+	//print_token_metadata(head);
 	free_tokens(&head);
+	replace_empty_cmds(result);
 	*dst = result;
 	print_t_listex(dst);
 	return (SUCCESS);
